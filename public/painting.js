e0bbf9e3e0edadda113061e8a07f16c05d879516
abulaziz bahamid <gimo999@hotmail.com>
2022-08-22 15:08:03 +0300

    creating the mvp

---
+++ public/painting.js
@@ -0,0 +1,51 @@
+let color = "red"
+const colors = document.getElementsByClassName("color");
+for (let index = 0; index < colors.length; index++) {
+  colors[index].addEventListener("click", ()=>{
+    color = colors[index].id;
+    document.getElementsByClassName("selected")[0].classList.toggle("selected");
+    colors[index].classList.toggle("selected");
+  });
+}
+
+
+
+const socket = io();
+socket.on("receive", (data)=>{
+  stroke(data.color)
+  strokeWeight(4)
+  line(data.x1, data.y1, data.x2, data.y2);
+})
+
+socket.on("join", (data)=>{
+  console.log(data.users);
+})
+
+socket.on("left", (data)=>{
+  console.log(data.users);
+})
+
+let sketchWidth = document.getElementById("canvas").offsetWidth;
+let sketchHeight = document.getElementById("canvas").offsetHeight;
+
+function setup() {
+  const myCanvas = createCanvas(sketchWidth, sketchHeight);
+  myCanvas.parent("canvas");
+  background(255);
+}
+
+function mouseDragged() {
+  stroke(color)
+  strokeWeight(4)
+  let data = {
+    color: color,
+    x1: mouseX,
+    y1: mouseY,
+    x2: pmouseX,
+    y2: pmouseY
+  };
+  console.log(pmouseX, data.x2);
+  console.log();
+  line(data.x1, data.y1, data.x2, data.y2);
+  socket.emit("send", data);
+}
--- server.js
+++ server.js
@@ -1,89 +1,30 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const socket = require("socket.io");
-const { fork } = require("child_process");
-const { dirname } = require("path");
-const controller = new AbortController();
-const { signal } = controller;
 const port = process.env.PORT || 3000;
-const io = socket(app.listen(port, () => {
+const io = socket(
+  app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
   })
 );
+usersOnline = 0;
 //middlewares
 app.use(express.static("public"));
-app.use(cookieParser());
-
-app.get("/a", (req,res)=>{
-  res.send(req.ip);
-});
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"public/homePage.html"));
+  res.sendFile(path.join(__dirname, "public/painting.html"));
 });
-
-app.get("/computeFibo/:number", (req, res) => {
-  computeFibo(res, req.params.number);
-});
-
-app.get("/chatroom", loginRequired, (req, res) => {
-  res.sendFile(path.join(__dirname, "public/chat.html"));
-});
-
-function loginRequired(req, res, next) {
-  const username = req.cookies.username;
-  // res.cookie("username","ahmedddd");
-  if (username) {
-    next();
-  } else {
-    res.redirect("/");
-  }
-}
 
 io.sockets.on("connection", (socket) => {
-  const username = parseCookie(socket.handshake.headers.cookie).username;
-
-  socket.broadcast.emit("receive", {
-    time: socket.handshake.headers.time,
-    username: username,
-    msg: "Entered tha chat!!!",
-  });
+  socket.emit("join", { users: ++usersOnline });
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("receive", {
-      time: "!!!!!",
-      username: username,
-      msg: "disconnected from the chat!!!",
-    });
+    usersOnline--;
+    socket.broadcast.emit("left");
   });
 
   socket.on("send", (data) => {
     socket.broadcast.emit("receive", data);
   });
 });
-
-function parseCookie(cookie) {
-  cookie = '{"' + cookie.replace("=", '":"') + '"}';
-  cookie = cookie.replace(";", ",");
-  cookie = JSON.parse(cookie);
-  return cookie;
-}
-
-
-function computeFibo(res, number) {
-  const fiboProcess = fork(__dirname + "/fibo.js", [`${number}`], { signal });
-
-  fiboProcess.on("error", (error) => {
-    res.sendStatus(500);
-  });
-
-  fiboProcess.on("message", (data) => {
-    res.send({ data });
-  });
-
-  fiboProcess.on("close", () => {
-    console.log(`im done from ${number}`);
-  });
-}
