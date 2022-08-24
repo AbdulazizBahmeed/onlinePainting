a3edf50648ffa66006e4d48fa1f88df1e290a753
abulaziz bahamid <gimo999@hotmail.com>
2022-08-24 10:37:40 +0300

    styling the page header and page title

--- public/painting.js
+++ public/painting.js
@@ -1,29 +1,28 @@
-let color = "red"
+let color = "red";
 const colors = document.getElementsByClassName("color");
 for (let index = 0; index < colors.length; index++) {
-  colors[index].addEventListener("click", ()=>{
-    color = colors[index].id;
-    document.getElementsByClassName("selected")[0].classList.toggle("selected");
-    colors[index].classList.toggle("selected");
-  });
+  colors[index].addEventListener("click", changeColor);
+}
+function changeColor() {
+  color = this.id;
+  document.getElementsByClassName("selected")[0].classList.toggle("selected");
+  this.classList.toggle("selected");
 }
 
-
-
 const socket = io();
-socket.on("receive", (data)=>{
-  stroke(data.color)
-  strokeWeight(4)
+socket.on("receive", (data) => {
+  stroke(data.color);
+  strokeWeight(4);
   line(data.x1, data.y1, data.x2, data.y2);
-})
+});
 
-socket.on("join", (data)=>{
+socket.on("join", (data) => {
   console.log(data.users);
-})
+});
 
-socket.on("left", ()=>{
+socket.on("left", () => {
   console.log("hello");
-})
+});
 
 let sketchWidth = document.getElementById("canvas").offsetWidth;
 let sketchHeight = document.getElementById("canvas").offsetHeight;
@@ -32,18 +31,18 @@ function setup() {
   const myCanvas = createCanvas(sketchWidth, sketchHeight);
   myCanvas.parent("canvas");
   background(255);
-  cursor("cursor.png")
+  cursor("cursor.png");
 }
 
 function mouseDragged() {
-  stroke(color)
-  strokeWeight(4)
+  stroke(color);
+  strokeWeight(4);
   let data = {
     color: color,
     x1: mouseX,
     y1: mouseY,
     x2: pmouseX,
-    y2: pmouseY
+    y2: pmouseY,
   };
   line(data.x1, data.y1, data.x2, data.y2);
   socket.emit("send", data);
